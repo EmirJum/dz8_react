5ea7daf4b102305ebce837aa16a075b334b7cbaf
EmirJum <mirkajum@gmail.com>
2022-07-09 14:46:12 +0600

    dz8

---
+++ my-app/src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
+import BlogsPage from "./pages/blogsPage/BlogsPage";
+import MainPage from "./pages/mainPage/MainPage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import Blog from "./components/blog/Blog";
+import LayOut from './components/outlet/LayOut';
+import LayOutBack from './components/outlet/LayOutBack';
+import About from './components/about/About';
+
+
+function App() {
+    return (
+        <BrowserRouter>
+            
+            <Routes>
+                <Route  path="/" element={<LayOut/>}>
+                  <Route index element={<MainPage/>}/>
+                  <Route path="blogs" element={<BlogsPage/>}/>
+                </Route>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/" element={<LayOutBack/>}>
+                  <Route path="/blogs/:id" element={<Blog/>}/>
+                  <Route path="about" element={<About/>}/>
+                </Route>
+                
+            </Routes>
+        </BrowserRouter>
+
+    );
+}
+
+export default App;
